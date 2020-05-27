667813918a91f14f8ab471ceeeb0851304d92b6c
Abi Akindele [abbeymart] <2366595+abbeymart@users.noreply.github.com>
2020-05-27 15:15:55 -0400

    first baseline commit

---
+++ mccrud.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "Abi Akindele [abbeymart]"
+description   = "mccrud package (nim)"
+license       = "MIT"
+srcDir        = "src"
+
+
+
+# Dependencies
+
+requires "nim >= 1.2.0"
---
+++ src/saverecord.nim
@@ -0,0 +1,9 @@
+#
+#            mconnect collections package
+#        (c) Copyright 2020 Abi Akindele (mconnect.biz)
+#
+#    See the file "LICENSE.md", included in this
+#    distribution, for details a bout the copyright / license.
+# 
+#                   CRUD Library - save record(s)
+# 
