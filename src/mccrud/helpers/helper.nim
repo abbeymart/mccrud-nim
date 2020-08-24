f4d84747b94c361c6560f9acfcbe7b11082e582e
Abi Akindele [abbeymart] <2366595+abbeymart@users.noreply.github.com>
2020-08-23 20:57:49 -0400

    refactoring crud components

---
+++ src/mccrud/helpers/helper.nim
@@ -0,0 +1,26 @@
+#
+#                   mconnect solutions
+#        (c) Copyright 2020 Abi Akindele (mconnect.biz)
+#
+#    See the file "LICENSE.md", included in this
+#    distribution, for details about the copyright / license.
+# 
+#                  CRUD Helper Procedures
+# 
+
+## CRUD helper procedures/functions for the CRUD operations
+##
+
+import computeSelect, computeWhere, computeUpdate, computeDelete
+import jsonToCrudSaveRecord, jsonToCrudReadRecord, jsonToCrudDeleteRecord, jsonToCrudUpdateRecord
+import auth, utils
+
+export computeSelect
+export computeWhere
+export computeUpdate
+export computeDelete
+export jsonToCrudSaveRecord
+export jsonToCrudReadRecord
+export jsonToCrudDeleteRecord
+export jsonToCrudUpdateRecord
+export auth, utils
